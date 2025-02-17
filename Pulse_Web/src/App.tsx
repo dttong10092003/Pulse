dbfe80873c8083236b638fd69f8068d76b67ab8c
DESKTOP-BPE6J3K\DELL <tong10092003@gmail.com>
2025-02-17 23:29:24 +0700

    Add page Login and Register

--- Pulse_Web/src/App.tsx
+++ Pulse_Web/src/App.tsx
@@ -1,18 +1,16 @@
-import Sidebar from "./components/Sidebar";
-import MainContent from "./components/MainContent";
-import RightSidebar from "./components/RightSidebar";
-import { useState } from "react";
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
 
-function App() {
-  const [isDark, setIsDark] = useState(true);
-
+const App = () => {
   return (
-    <div className={`${isDark ? "bg-[#1F1F1F] text-white" : "bg-white text-black"} min-h-screen flex`}>
-      <Sidebar isDark={isDark} setIsDark={setIsDark} />
-      <MainContent isDark={isDark} />
-      <RightSidebar isDark={isDark} />
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
