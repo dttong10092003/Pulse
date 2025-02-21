94612bd285d1941cd7659f6d726d19a35651628d
tinph010803 <tinphan924@gmail.com>
2025-02-21 10:33:50 +0700

    fix Sidebar & add Explore

--- Pulse_Web/src/pages/Home.tsx
+++ Pulse_Web/src/pages/Home.tsx
@@ -4,27 +4,30 @@ import MyProfile from "./profile/MyProfile";
 import RightSidebar from "../components/RightSidebar";
 import Message from "./message/Message";
 import EditProfile from "./profile/EditProfile";
+import Explore from "./explore/Explore";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 const Home = () => {
   const location = useLocation();
-  console.log(location.pathname);
-
   const isMessagePage = location.pathname === "/home/message";
 
   return (
-    <div className={` bg-[#1F1F1F] text-white min-h-screen flex`}>
+    <div className="flex bg-[#1F1F1F] text-white min-h-screen">
+      {/* Sidebar cố định */}
       <Sidebar />
-      
-      {/* Chỉ thay đổi phần nội dung chính */}
-      <div className={`flex-1`}>
-      <Routes>
-        <Route path="/" element={<MainContent/>} />
-        <Route path="/my-profile" element={<MyProfile  />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
-        <Route path="/message" element={<Message />} />
-      </Routes>
+
+      {/* Nội dung chính, tránh bị che bằng `ml-72` */}
+      <div className="flex-1 ml-72 overflow-auto p-6">
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/my-profile" element={<MyProfile />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+          <Route path="/message" element={<Message />} />
+          <Route path="/explore" element={<Explore />} />
+        </Routes>
       </div>
+
+      {/* Right Sidebar chỉ hiển thị nếu không phải trang Message */}
       {!isMessagePage && <RightSidebar />}
     </div>
   );
