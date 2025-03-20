abb7cc34e3b12ae19d1766a4a69d190511e3b682
tinph010803 <tinphan924@gmail.com>
2025-03-20 11:53:02 +0700

    add userinfo

--- Pulse_Web/src/App.tsx
+++ Pulse_Web/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import Home from './pages/Home'
+import UserInfo from "./pages/userInfo/UserInfo";
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/userinfo" element={<UserInfo />} />
       <Route path="/home/*" element={<Home />} /> {/* thêm để quản lý các route con */}
     </Routes>
     </Provider>
