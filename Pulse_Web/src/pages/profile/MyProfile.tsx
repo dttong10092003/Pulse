5b2029fdf973100dcf52eb6912fb7b866e2db7ba
tinph010803 <tinphan924@gmail.com>
2025-04-24 10:26:34 +0700

    update media my profile

--- Pulse_Web/src/pages/profile/MyProfile.tsx
+++ Pulse_Web/src/pages/profile/MyProfile.tsx
@@ -152,106 +152,103 @@ const MyProfile = () => {
                 </button>
             </div>
             {/* New Post Input Section */}
-            <div
-                ref={inputRef}
-                className={`p-4 bg-[#1F1F1F] rounded-2xl transition-all duration-300 mt-4 mx-4 ${isExpanded ? "h-auto" : "h-16"
-                    } flex flex-col gap-3`}
-            >
-                <div className="flex items-start gap-4">
-                    <img
-                        src={userDetail?.avatar}
-                        alt="Avatar"
-                        className="w-10 h-10 rounded-full object-cover"
-                    />
-                    <div className="flex-1">
-                        <textarea
-                            placeholder="Start a post..."
-                            className="w-full bg-transparent text-lg placeholder:text-zinc-500 focus:outline-none text-zinc-200 resize-none overflow-hidden"
-                            style={{ minHeight: "40px" }}
-                            rows={isExpanded ? 3 : 1}
-                            onFocus={() => setIsExpanded(true)}
-                            value={postContent}
-                            onChange={(e) => setPostContent(e.target.value)}
+            {activeTab === "Posts" && (
+                <div
+                    ref={inputRef}
+                    className={`p-4 bg-[#1F1F1F] rounded-2xl transition-all duration-300 mt-4 mx-4 ${isExpanded ? "h-auto" : "h-16"
+                        } flex flex-col gap-3`}
+                >
+                    <div className="flex items-start gap-4">
+                        <img
+                            src={userDetail?.avatar}
+                            alt="Avatar"
+                            className="w-10 h-10 rounded-full object-cover"
                         />
-                        {mediaFiles.length > 0 && (
-                            <div className="mt-2 grid grid-cols-2 gap-2 max-w-full">
-                                {mediaFiles.map((file, index) => (
-                                    <div key={index} className="relative w-full max-w-sm">
-                                        <button
-                                            onClick={() =>
-                                                setMediaFiles((prev) => prev.filter((_, i) => i !== index))
-                                            }
-                                            className="absolute top-2 right-2 bg-black/60 text-white rounded-full px-2 py-1 text-sm z-10 hover:bg-black/80 cursor-pointer"
-                                        >
-                                            <X size={16} />
-                                        </button>
+                        <div className="flex-1">
+                            <textarea
+                                placeholder="Start a post..."
+                                className="w-full bg-transparent text-lg placeholder:text-zinc-500 focus:outline-none text-zinc-200 resize-none overflow-hidden"
+                                style={{ minHeight: "40px" }}
+                                rows={isExpanded ? 3 : 1}
+                                onFocus={() => setIsExpanded(true)}
+                                value={postContent}
+                                onChange={(e) => setPostContent(e.target.value)}
+                            />
+                            {mediaFiles.length > 0 && (
+                                <div className="mt-2 grid grid-cols-2 gap-2 max-w-full">
+                                    {mediaFiles.map((file, index) => (
+                                        <div key={index} className="relative w-full max-w-sm">
+                                            <button
+                                                onClick={() =>
+                                                    setMediaFiles((prev) => prev.filter((_, i) => i !== index))
+                                                }
+                                                className="absolute top-2 right-2 bg-black/60 text-white rounded-full px-2 py-1 text-sm z-10 hover:bg-black/80 cursor-pointer"
+                                            >
+                                                <X size={16} />
+                                            </button>
 
-                                        {file.type.startsWith("image/") ? (
-                                            <img
-                                                src={URL.createObjectURL(file)}
-                                                alt={`media-${index}`}
-                                                className="w-full max-h-80 object-cover rounded"
-                                                style={{ aspectRatio: '16/9' }}
-                                            />
-                                        ) : (
-                                            <video
-                                                src={URL.createObjectURL(file)}
-                                                controls
-                                                className="w-full max-h-80 object-cover rounded"
-                                                style={{ aspectRatio: '16/9' }}
-                                            />
-                                        )}
-                                    </div>
-                                ))}
-                            </div>
-                        )}
-
-
-
-                    </div>
-                </div>
-
-                {isExpanded && (
-                    <div className="flex items-center justify-between mt-2">
-                        <div className="flex items-center gap-2">
-                            <>
-                                <button
-                                    className="flex items-center justify-center bg-zinc-700 hover:bg-zinc-600 p-2 rounded-lg transition cursor-pointer text-white"
-                                    onClick={() => fileInputRef.current?.click()}
-                                >
-                                    <Image size={20} className="text-white" />
-                                </button>
-                                <input
-                                    type="file"
-                                    accept="image/*,video/*"
-                                    className="hidden"
-                                    ref={fileInputRef}
-                                    onChange={handleMediaSelect}
-                                    multiple
-                                />
-
-                            </>
-
-                            <div className="flex items-center gap-2 bg-zinc-700 hover:bg-zinc-600 px-3 py-2 rounded-lg text-white cursor-pointer ">
-                                <div className="w-3 h-3 bg-pink-500 rounded-full"></div>
-                                <span>Beauty</span>
-                                <ChevronDown size={16} />
-                            </div>
+                                            {file.type.startsWith("image/") ? (
+                                                <img
+                                                    src={URL.createObjectURL(file)}
+                                                    alt={`media-${index}`}
+                                                    className="w-full max-h-80 object-cover rounded"
+                                                    style={{ aspectRatio: '16/9' }}
+                                                />
+                                            ) : (
+                                                <video
+                                                    src={URL.createObjectURL(file)}
+                                                    controls
+                                                    className="w-full max-h-80 object-cover rounded"
+                                                    style={{ aspectRatio: '16/9' }}
+                                                />
+                                            )}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
                         </div>
-                        <button
-                            className={`px-5 py-2 rounded-3xl transition ${postContent.trim() && !isPosting
+                    </div>
+
+                    {isExpanded && (
+                        <div className="flex items-center justify-between mt-2">
+                            <div className="flex items-center gap-2">
+                                <>
+                                    <button
+                                        className="flex items-center justify-center bg-zinc-700 hover:bg-zinc-600 p-2 rounded-lg transition cursor-pointer text-white"
+                                        onClick={() => fileInputRef.current?.click()}
+                                    >
+                                        <Image size={20} className="text-white" />
+                                    </button>
+                                    <input
+                                        type="file"
+                                        accept="image/*,video/*"
+                                        className="hidden"
+                                        ref={fileInputRef}
+                                        onChange={handleMediaSelect}
+                                        multiple
+                                    />
+                                </>
+                                <div className="flex items-center gap-2 bg-zinc-700 hover:bg-zinc-600 px-3 py-2 rounded-lg text-white cursor-pointer ">
+                                    <div className="w-3 h-3 bg-pink-500 rounded-full"></div>
+                                    <span>Beauty</span>
+                                    <ChevronDown size={16} />
+                                </div>
+                            </div>
+                            <button
+                                className={`px-5 py-2 rounded-3xl transition ${postContent.trim() && !isPosting
                                     ? "bg-green-500 hover:bg-green-400 text-white cursor-pointer"
                                     : "bg-zinc-600 text-white opacity-50 cursor-not-allowed"
-                                }`}
-                            disabled={!postContent.trim() || isPosting}
-                            onClick={handleCreatePost}
-                        >
-                            {isPosting ? "Posting..." : "Post"}
-                        </button>
+                                    }`}
+                                disabled={!postContent.trim() || isPosting}
+                                onClick={handleCreatePost}
+                            >
+                                {isPosting ? "Posting..." : "Post"}
+                            </button>
+                        </div>
+                    )}
+                </div>
+            )}
 
-                    </div>
-                )}
-            </div>
             <div className="mt-4">
                 {activeTab === "Posts" && <Posts posts={userPosts} username={username} avatar={userDetail?.avatar ?? "default-avatar-url"} />}
                 {activeTab === "Featured" && <Featured />}
